b6e12c8df178c02a5c8d249fdd9bbe11420f797c
Sam Woodman <swoodman213@gmail.com>
2019-11-17 09:33:45 -0800

    Cleaned up airdas_sight code. Doc still needs work

--- R/airdas_sight.R
+++ R/airdas_sight.R
@@ -12,19 +12,29 @@
 #' @importFrom dplyr left_join
 #' @importFrom dplyr mutate
 #' @importFrom dplyr select
-#' @importFrom dplyr slice
+#' @importFrom rlang !!
 #'
-#' @details TODO. 
-#'   TODO: make parameters for 'event.sight' and 'event.turtle'?
-#'   Marine mammal sighting codes: "S" and "s"; turtle sighting code: "t"
+#' @details TODO: make parameters for 'event.sight', 'event.turtle', and 'event.sight.info'?
+#'   
+#'   Marine mammal sighting codes: "S" (first sighting) and "s" (resighting); 
+#'   turtle sighting code: "t";
+#'   additional sighting information: "1"
+#'   
+#'   See ... todo (link to pdf) more info
+#'   
 #'   This function assumes the following 
 #'   \tabular{llll}{
-#'     \emph{Information} \tab \emph{Mammal sighting} \tab 
-#'       \emph{Turtle sighting} \tab \emph{New column name}\cr
+#'     \emph{Information} \tab \emph{Mammal sighting} \tab \emph{Turtle sighting} \tab \emph{New column name}\cr
 #'     Sighting number \tab Data1 \tab \tab sight_no\cr
 #'     Observer \tab Data2 \tab Data1 \tab obs\cr
 #'     Declination angle \tab Data3 \tab Data2 \tab angle_declination\cr
-#'     Etc\cr
+#'     Group size (best estimate) \tab Data4 \tab NA \tab todo \cr
+#'     Species code 1 \tab Data5 \tab Data3 \tab todo \cr
+#'     Species code 2 \tab Data6 \tab NA \tab todo \cr
+#'     Species code 3 \tab Data7 \tab NA \tab todo \cr
+#'     Turtle size (ft) \tab NA \tab Data4 \tab todo\cr
+#'     Travel direction (deg) \tab NA \tab Data5 \tab todo\cr
+#'     Tail visible? \tab NA \tab Data6 \tab todo\cr
 #'   }
 #'
 #' @return Data frame with one row for each species for each sighting.
@@ -37,52 +47,61 @@
 #' # d <- do.call(rbind, lapply(list.files("../airdas/airDAS_files", full.names = TRUE), airdas_read))
 #' # x <- airdas_process(d)
 #' # x.sight <- airdas_sight(x)
+#' 
+#' d.proc <- airdas_process(system.file("airdas_sample.das", package = "swfscAirDAS"))
+#' airdas_sight(d.proc)
 #'
 #' @export
 airdas_sight <- function(das.df) {
   #----------------------------------------------------------------------------
   # Filter for and extract sighting data
-  das.df.orig <- das.df
   event.sight <- c("S", "s", "t")
+  event.sight.info <- c("1")
   
   ### Filter for sighting-related data
-  das.df <- das.df.orig %>% 
-    filter(.data$Event %in% c(event.sight, 1)) %>% 
+  sight.df <- das.df %>% 
+    filter(.data$Event %in% c(event.sight, event.sight.info)) %>% 
     mutate(sight_cumsum = cumsum(.data$Event %in% event.sight))
+  
+  stopifnot(length(event.sight.info) == 1)
   
   
   #----------------------------------------------------------------------------
   ### For every sighting event paired with a '1' event, split the sighting
-  ###   into multiple lines
-  sight.cumsum.mult <- das.df$sight_cumsum[das.df$Event == 1]
-  sight.mult <- lapply(sight.cumsum.mult, function(i, das.df) {
-    curr.df <- das.df %>% filter(.data$sight_cumsum == i)
-    stopifnot(
-      nrow(curr.df) == 2, 
-      identical(curr.df$Event, c("S", "1"))
-    )
-    
+  ###   into multiple lines. 
+  ### If no '1' events, sight.mult will be list() and thus not change sight.df
+  sight.cumsum.mult <- sight.df$sight_cumsum[sight.df$Event %in% event.sight.info]
+  sight.mult <- lapply(sight.cumsum.mult, function(i, sight.df) {
+    curr.df <- sight.df %>% filter(.data$sight_cumsum == i)
+    stopifnot(identical(curr.df$Event, c("S", "1")))
+
+    # Extract species and group size info
     gs.total <- as.numeric(curr.df$Data4[1])
     sp.all <- c(curr.df$Data5[1], curr.df$Data6[1], curr.df$Data7[1])
-    sp.num.all <- c(
-      as.numeric(curr.df$Data5[2]) / 100 * gs.total, 
-      as.numeric(curr.df$Data6[2]) / 100 * gs.total, 
-      as.numeric(curr.df$Data7[2]) / 100 * gs.total
+    sp.perc.all <- c(
+      as.numeric(curr.df$Data5[2]), as.numeric(curr.df$Data6[2]), 
+      as.numeric(curr.df$Data7[2])
     )
+    sp.num.all <- sp.perc.all / 100 * gs.total
     
-    # TODO: make warning message more informative
-    if (!all.equal(sum(sp.num.all  * 100 / gs.total, na.rm = TRUE), 100))
-      warning("Some of the multispecies sightings percentages ", 
-              "do not sum to 100")
+    # Warning if species percentages do not sum to 100
+    if (!all.equal(sum(sp.perc.all, na.rm = TRUE), 100)) {
+      file.line <- paste0(
+        "file '", curr.df$file_das[1], "', line '", curr.df$line_num[2], "'"
+      )
+      warning("The multispecies sightings percentages for ", file.line, 
+              ", do not sum to 100", immediate. = TRUE)
+    }
     
+    # Create df with one row for each species
     rbind(curr.df[1, ], curr.df[1, ], curr.df[1, ]) %>% 
       mutate(Data4 = as.character(sp.num.all), Data5 = sp.all, 
              Data6 = NA, Data7 = NA) %>% 
-      slice(which(!is.na(sp.all)))
-  }, das.df = das.df)
+      filter(!is.na(.data$EData4))
+  }, sight.df = sight.df)
   
-  # Add 'new' sightings back into das.df
-  das.df <- das.df %>% 
+  # Add 'new' sightings back into sight.df
+  sight.df <- sight.df %>% 
     filter(!(.data$sight_cumsum %in% sight.cumsum.mult)) %>% 
     bind_rows(do.call(rbind, sight.mult)) %>% 
     arrange(.data$sight_cumsum)
@@ -91,15 +110,15 @@ airdas_sight <- function(das.df) {
   
   # Ensure that there is no "S" event data in Data6 or Data7
   stopifnot(
-    all(is.na(filter(das.df, .data$Event == "S")$Data6)), 
-    all(is.na(filter(das.df, .data$Event == "S")$Data7))
+    all(is.na(filter(sight.df, .data$Event == "S")$Data6)), 
+    all(is.na(filter(sight.df, .data$Event == "S")$Data7))
   )
   
   
   #----------------------------------------------------------------------------
   ### Extract relevant sighting data for...
   # All sighting events
-  sight.info.s <- das.df %>% 
+  sight.info.s <- sight.df %>% 
     filter(.data$Event %in% event.sight) %>% 
     mutate(sight_no = ifelse(.data$Event == "t", NA, .data$Data1), 
            obs = ifelse(.data$Event == "t", .data$Data1, .data$Data2), 
@@ -111,7 +130,7 @@ airdas_sight <- function(das.df) {
   
   # Only t events. Must be filtered for t so that multispecies lines are not
   #   duplicated in final join
-  sight.info.t <- das.df %>% 
+  sight.info.t <- sight.df %>% 
     filter(.data$Event == "t") %>% 
     mutate(turtle_sizeft = .data$Data4, 
            turtle_direction = .data$Data5, 
