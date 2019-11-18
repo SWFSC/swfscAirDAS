27a484df51bd5421b7c9746837af9118d5919c99
Sam Woodman <swoodman213@gmail.com>
2019-11-18 14:33:59 -0800

    _copy function name seemed misleading; streamlines docs

---
+++ R/airdas_format_pdf.R
@@ -0,0 +1,30 @@
+#' Aerial DAS format requirements
+#'
+#' Save the pdf document describing the aerial DAS format required by \code{swfscAirDAS} to a specified file
+#'
+#' @param file character vector, the name of the file where the pdf will be saved
+#' @param ... arguments passed to \link[base]{file.copy}, such as \code{overwrite}
+#'
+#' @details A wrapper function for \link[base]{file.copy}. This function
+#'   saves the pdf document describing the aerial DAS data format requirements by 
+#'   copying the pdf document located at \code{system.file("AirDAS_Format.pdf", package = "swfscAirDAS")}
+#'   to \code{file}
+#'
+#' @return \code{TRUE} if writing of file was successful, and \code{FALSE} otherwise
+#'
+#' @examples
+#' \dontrun{
+#' airdas_format_pdf("AirDAS_Format.pdf", overwrite = FALSE)
+#' }
+#'
+#' @export
+airdas_format_pdf <- function(file, ...) {
+  stopifnot(
+    inherits(file, "character"), 
+    length(file) == 1
+  )
+  
+  file.copy(
+    system.file("AirDAS_Format.pdf", package = "swfscAirDAS"), to = file, ...
+  )
+}
