052f3a56f5dc142b5516195adfc8c25fd46fe20a
Sam Woodman <swoodman213@gmail.com>
2020-02-13 12:09:30 -0800

    Finished docs for airdas_chop_condition, and updated them for readme and airdas_chop_equal

--- R/airdas_chop_condition.R
+++ R/airdas_chop_condition.R
@@ -1,40 +1,55 @@
 #' Chop AirDAS data - condition
 #' 
-#' Chop AirDAS data into a new effort segment every time any condition changes
+#' Chop AirDAS data into a new effort segment every time a condition changes
 #' 
 #' @param x \code{airdas_df} object, 
 #'   or a data frame that can be coerced to a \code{airdas_df} object. 
 #'   This data must be filtered for 'OnEffort' events; 
 #'   see the Details section below
+#' @param seg.km.min numeric; minimum allowable segment length (in kilometers).
+#'   Default is 0.1. See the Details section below for more information
 #' @param ... ignored
-#' @param seg.km.min numeric; default is 0.1. 
-#'   All segments shorter than this distance, which is in kilometers, 
-#'   will be smushed with the segment immediately following them. 
-#'   If \code{seg.km.min} is \code{0}, then only event series (e.g. TVPAW) will be smushed
 #'   
-#' @importFrom dplyr %>% filter left_join mutate select
+#' @importFrom dplyr %>% filter group_by left_join mutate select summarise
 #' @importFrom swfscMisc distance
 #' @importFrom utils head
 #' 
-#' @details This function is intended to be called by \code{\link{airdas_effort}} 
+#' @details This function is intended to only be called by \code{\link{airdas_effort}} 
 #'   when the "condition" method is specified. 
 #'   Thus, \code{x} must be filtered for events (rows) where either
 #'   the 'OnEffort' column is \code{TRUE} or the 'Event' column is "E" or "O"; 
 #'   see \code{\link{airdas_effort}} for more details. 
-#'   Continuous effort sections (henceforth 'effort sections') from \code{x}
-#'   are chopped into modeling segments (henceforth 'segments') every time a condition changes 
+#'   This function chops each continuous effort section (henceforth 'effort sections') 
+#'   in \code{x} into modeling segments (henceforth 'segments') by 
+#'   creating a new segment every time a condition changes. 
 #'   Each effort section runs from a T/R event to its corresponding E/O event. 
-#'   Then \code{\link{airdas_segdata_avg}} is called to get relevant segdata information.
+#'   After chopping, \code{\link{airdas_segdata_avg}} is called to get relevant  
+#'   segdata information for each segment.
 #'   
-#'   The following conditions trigger the creation of a new segment:
-#'   viewing conditions, altitude, speed, HKR, percent overcast (cloud cover), 
-#'   Beaufort, Jellyfish code, and horizontal sun
+#'   Changes in the following conditions trigger a new segment:
+#'   Beaufort, percent overcast (cloud cover), jellyfish code, horizontal sun
+#'   altitude, speed, haze, kelp, red tide, and observer viewing conditions.
+#'   The main exception is when multiple condition changes happen at 
+#'   the same location, such as a 'TVPAW' series of events. 
+#'   When this happens, no segments of length zero are created; 
+#'   rather, a single segment is created that includes all of the condition changes 
+#'   (i.e. all of the events in the event series) that happened during 
+#'   the series of events (i.e. at the same location). 
 #'   
-#'   All segments with length zero will be smushed with the segment immediately following them.
-#'   This will take care of event series that all have the same location, such as TVPAW.
-#'   Exceptions to \code{seg.km.min} are segments at the end of a continuous effort section
+#'   In addition, (almost) all segments whose length is less than \code{seg.km.min}
+#'   are combined with the segment immediately following them to ensure that the length
+#'   of (almost) all segments is at least \code{seg.km.min}. 
+#'   This allows users to account for situations where multiple conditions, 
+#'   such as Beaufort and the viewing conditions, change in rapid succession, say 0.05 km apart.
+#'   When segments are combined, a warning is thrown and the conditions are averaged together 
+#'   across the now-larger segment (question for Karin - is this right?). 
+#'   The only exception to this rule is if the short segment ends in an "E" or O" event, 
+#'   meaning it is the last segment of the effort section. 
+#'   Since in this case there is no 'next' segment, this segment is left as-is.
 #'   
-#'   TODO
+#'   Note that the above rule for 'combining' condition changes that have the same location 
+#'   into a single segment (such as a 'TVPAW' series of events) 
+#'   is followed even if \code{seg.km.min = 0}.
 #'   
 #' @return List of two data frames:
 #' \itemize{
@@ -58,7 +73,7 @@ airdas_chop_condition.data.frame <- function(x, ...) {
 
 #' @name airdas_chop_condition
 #' @export
-airdas_chop_condition.airdas_df <- function(x, seg.km.min = 0.1, ...){
+airdas_chop_condition.airdas_df <- function(x, seg.km.min = 0.1, ...) {
   #----------------------------------------------------------------------------
   # Input checks
   if (!all(x$OnEffort | x$Event %in% c("O", "E"))) 
@@ -134,6 +149,7 @@ airdas_chop_condition.airdas_df <- function(x, seg.km.min = 0.1, ...){
     seg.len0 <- d.pre$idx_end[d.pre$dist_length == 0] + 1
     seg.len1 <- d.pre$idx_end[d.pre$dist_length < seg.km.min] + 1
     seg.diff <- setdiff(seg.len1, seg.len0)
+    # TODO: use internals: .greater()
     if (length(seg.diff) > 0 & all(seg.diff <= nrow(das.df)))
       warning("Because of smushing, not all conditions are the same ", 
               "for each segment in continuous effort section ", i, 
